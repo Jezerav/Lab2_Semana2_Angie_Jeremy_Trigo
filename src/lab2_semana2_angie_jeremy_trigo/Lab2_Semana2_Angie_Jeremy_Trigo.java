f7c29d1d6ebf9cbb2a119d786c51f3d231df726e
Jezerav <jeremy.robateau09@gmail.com>
2025-10-24 08:44:00 -0600

    Creacion Repositorio

---
+++ src/lab2_semana2_angie_jeremy_trigo/Lab2_Semana2_Angie_Jeremy_Trigo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab2_semana2_angie_jeremy_trigo;
+
+/**
+ *
+ * @author jerem
+ */
+public class Lab2_Semana2_Angie_Jeremy_Trigo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
